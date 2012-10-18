dfdd9d003777b1d797cb2b81d0ebd43ac45cdffe
Charlie Root <root@mokryak.com>
2012-10-18 19:19:54 +0400

    changes

---
+++ mokryak.com/core/cache/mgr/smarty/default/af1f9d3da23df0be8a1f6a16c32cba72b7132fe6.file.update.tpl.php
@@ -0,0 +1,21 @@
+<?php /* Smarty version Smarty-3.0.4, created on 2012-10-17 19:57:27
+         compiled from "/home/admin/data/www/mokryak.com/manager/templates/default/element/template/update.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:66635702507ed5673d43c6-92889550%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'af1f9d3da23df0be8a1f6a16c32cba72b7132fe6' => 
+    array (
+      0 => '/home/admin/data/www/mokryak.com/manager/templates/default/element/template/update.tpl',
+      1 => 1350479528,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '66635702507ed5673d43c6-92889550',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+<div id="modx-panel-template-div"></div>
+<?php echo $_smarty_tpl->getVariable('onTempFormPrerender')->value;?>
---
+++ mokryak.com/core/cache/mgr/smarty/default/eb20cea3938d24863321ab6738193d178ed79c58.file.update.tpl.php
@@ -0,0 +1,41 @@
+<?php /* Smarty version Smarty-3.0.4, created on 2012-10-17 19:56:59
+         compiled from "/home/admin/data/www/mokryak.com/manager/templates/default/resource/update.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:1018939394507ed54bdc0ce0-81433696%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'eb20cea3938d24863321ab6738193d178ed79c58' => 
+    array (
+      0 => '/home/admin/data/www/mokryak.com/manager/templates/default/resource/update.tpl',
+      1 => 1350479529,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '1018939394507ed54bdc0ce0-81433696',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+<?php if (!is_callable('smarty_modifier_escape')) include '/home/admin/data/www/mokryak.com/core/model/smarty/plugins/modifier.escape.php';
+?><div id="modx-panel-resource-div"></div>
+<div id="modx-resource-tvs-div"><?php echo $_smarty_tpl->getVariable('tvOutput')->value;?>
+</div>
+<?php  $_smarty_tpl->tpl_vars['tv'] = new Smarty_Variable;
+ $_from = $_smarty_tpl->getVariable('hidden')->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+if ($_smarty_tpl->_count($_from) > 0){
+    foreach ($_from as $_smarty_tpl->tpl_vars['tv']->key => $_smarty_tpl->tpl_vars['tv']->value){
+?>
+    <input type="hidden" id="tvdef<?php echo $_smarty_tpl->getVariable('tv')->value->id;?>
+" value="<?php echo smarty_modifier_escape($_smarty_tpl->getVariable('tv')->value->default_text);?>
+" />
+    <?php echo $_smarty_tpl->getVariable('tv')->value->get('formElement');?>
+
+<?php }} ?>
+
+<?php echo $_smarty_tpl->getVariable('onDocFormPrerender')->value;?>
+
+<?php if ($_smarty_tpl->getVariable('resource')->value->richtext&&(isset($_smarty_tpl->getVariable('_config')->value['use_editor']) ? $_smarty_tpl->getVariable('_config')->value['use_editor'] : null)){?>
+<?php echo $_smarty_tpl->getVariable('onRichTextEditorInit')->value;?>
+
+<?php }?>
